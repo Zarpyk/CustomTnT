cf88d0e724fb8a4d0d46d0a32924493b359014c5
Zarpyk <30746531+Zarpyk@users.noreply.github.com>
2020-07-11 12:38:31 +0200

    Fix

--- src/guerrero61/tntcore/Main.java
+++ src/guerrero61/tntcore/Main.java
@@ -49,7 +49,6 @@ public class Main extends JavaPlugin {
 	
 	
 	public void onEnable() {
-		//F
 		Bukkit.getConsoleSender().sendMessage(startMessage);
 		if (CheckDisablePlugin(getPluginLoader(), (Plugin) this)) {
 			registerConfig();
